aecb06650ee4d541a41351b04226e2070f5601c2
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2024-04-05 11:43:42 +0100

    tag support

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 	"github.com/gosimple/slug"
 	"github.com/pnmcosta/csta.dev/internal/posts"
 	"github.com/pnmcosta/csta.dev/internal/templates/index"
-	templates "github.com/pnmcosta/csta.dev/internal/templates/post"
+	postTempl "github.com/pnmcosta/csta.dev/internal/templates/post"
+	tagTempl "github.com/pnmcosta/csta.dev/internal/templates/tag"
 )
+
+type Post = posts.Post
 
 var devFlag = flag.Bool("dev", false, "if true public folder will be served")
 
@@ -23,10 +26,72 @@ func main() {
 
 	// Output path.
 	rootPath := "public"
-	if err := os.Mkdir(rootPath, 0755); err != nil {
-		if !os.IsExist(err) {
-			log.Fatalf("failed to create output directory: %v", err)
+	if err := os.Mkdir(rootPath, 0755); err != nil && !os.IsExist(err) {
+		log.Fatalf("failed to create output directory: %v", err)
+	}
+
+	tags := map[string][]Post{}
+
+	// Create a page for each post.
+	for _, post := range posts {
+		// Create the output directory.
+		dir := path.Join(rootPath, post.Date.Format("2006/01/02"), slug.Make(post.Title))
+		if err := os.MkdirAll(dir, 0755); err != nil && !os.IsExist(err) {
+			log.Fatalf("failed to create dir %q: %v", dir, err)
+			continue
 		}
+
+		// Create the output file.
+		name := path.Join(dir, "index.html")
+		f, err := os.Create(name)
+		if err != nil {
+			log.Fatalf("failed to create output file: %v", err)
+			continue
+		}
+
+		// Create an unsafe component containing raw HTML.
+		content := postTempl.Unsafe(string(post.Content))
+
+		// Use templ to render the template containing the raw HTML.
+		err = postTempl.View(post, content).Render(context.Background(), f)
+		if err != nil {
+			log.Fatalf("failed to write output file: %v", err)
+			continue
+		}
+
+		for _, tag := range post.Tags {
+			tags[tag] = append(tags[tag], post)
+		}
+	}
+
+	// Create a page for each tag.
+	tagKeys := make([]string, 0, len(tags))
+	for tag, posts := range tags {
+		slug := slug.Make(tag)
+
+		// Create the output directory.
+		dir := path.Join(rootPath, "tag", slug)
+		if err := os.MkdirAll(dir, 0755); err != nil && !os.IsExist(err) {
+			log.Fatalf("failed to create dir %q: %v", dir, err)
+			continue
+		}
+
+		// Create the output file.
+		name := path.Join(dir, "index.html")
+		f, err := os.Create(name)
+		if err != nil {
+			log.Fatalf("failed to create output file: %v", err)
+			continue
+		}
+
+		// Use templ to render the template containing the raw HTML.
+		err = tagTempl.View(tag, posts).Render(context.Background(), f)
+		if err != nil {
+			log.Fatalf("failed to write output file: %v", err)
+			continue
+		}
+
+		tagKeys = append(tagKeys, tag)
 	}
 
 	// Create an index page.
@@ -37,42 +102,18 @@ func main() {
 	}
 
 	// Write it out.
-	err = index.View(posts).Render(context.Background(), f)
+	err = index.View(posts, tagKeys).Render(context.Background(), f)
 	if err != nil {
 		log.Fatalf("failed to write index page: %v", err)
-	}
-
-	// Create a page for each post.
-	for _, post := range posts {
-		// Create the output directory.
-		dir := path.Join(rootPath, post.Date.Format("2006/01/02"), slug.Make(post.Title))
-		if err := os.MkdirAll(dir, 0755); err != nil && err != os.ErrExist {
-			log.Fatalf("failed to create dir %q: %v", dir, err)
-		}
-
-		// Create the output file.
-		name := path.Join(dir, "index.html")
-		f, err := os.Create(name)
-		if err != nil {
-			log.Fatalf("failed to create output file: %v", err)
-		}
-
-		// Create an unsafe component containing raw HTML.
-		content := templates.Unsafe(string(post.Content))
-
-		// Use templ to render the template containing the raw HTML.
-		err = templates.View(post, content).Render(context.Background(), f)
-		if err != nil {
-			log.Fatalf("failed to write output file: %v", err)
-		}
 	}
 
 	if !*devFlag {
 		return
 	}
 
+	// Static serve only on dev
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Print("Listening on 127.0.0.1:3000...")
+	log.Println("Listening on 127.0.0.1:3000")
 	err = http.ListenAndServe("127.0.0.1:3000", nil)
 	if err != nil {
 		log.Fatal(err)
